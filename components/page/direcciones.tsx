5d071082f2c06ff654e4082b3f330601185c3692
David Schiro <david.schiro@devbase.us>
2026-01-06 15:51:45 -0300

    First commit

---
+++ components/page/direcciones.tsx
@@ -0,0 +1,84 @@
+import { Church, Clock, MapPin, PartyPopper } from 'lucide-react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+
+export default function Direcciones() {
+  return (
+    <section
+      id="direcciones"
+      className="w-full flex flex-col gap-5 items-center justify-center my-page"
+    >
+      <h1>Información y direcciones</h1>
+
+      <div className="flex flex-col md:flex-row gap-10 w-3/4">
+        <Card className="flex w-full flex-col md:w-1/2">
+          <CardHeader>
+            <CardTitle className="flex flex-row items-center gap-5">
+              <Church /> <p>La Iglesia</p>
+            </CardTitle>
+            <CardDescription className="flex flex-row items-center gap-2">
+              <Clock size="1rem" /> Horario: 11.45 hs
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grow">
+            La iglesia avenida Hipolito Yrigoyen 250, General Pacheco (Código
+            Postal 1617), Buenos Aires.
+            <br />
+            <br />
+            La ceremonia se celebrará aquí, revisa la dirección fácil y rápido
+            cuantas veces necesites. ¡Te esperamos!
+          </CardContent>
+          <CardFooter>
+            <Link
+              href="https://maps.app.goo.gl/HKHefriitkE1fdoh8"
+              target="_blank"
+            >
+              <Button>
+                <MapPin />
+                Abrir en Google Maps
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+
+        <Card className="flex w-full flex-col md:w-1/2">
+          <CardHeader>
+            <CardTitle className="flex flex-row items-center gap-5">
+              <PartyPopper /> <p>¡La Fiesta!</p>
+            </CardTitle>
+            <CardDescription className="flex flex-row items-center gap-2">
+              <Clock size="1rem" /> Horario: Desde las 14.00 a 22.00 hs
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grow">
+            ¡Luego de la ceremonia nos reuniremos aquí para seguir celebrando
+            juntos este nuevo comienzo!
+            <br />
+            <br />
+            Ubicado en: Cordoba 1233, Ingeniero Maschwitz (Código postal 1623),
+            Buenos Aires
+          </CardContent>
+          <CardFooter>
+            <Link
+              href="https://maps.app.goo.gl/FKQMjpzmVVE5abLA9"
+              target="_blank"
+            >
+              <Button>
+                <MapPin />
+                Abrir en Google Maps
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
+}
---
+++ components/page/header.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import Image from 'next/image';
+import { Card, CardContent } from '../ui/card';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+
+export default function Header() {
+  return (
+    <section id="header" className="flex h-[90svh] flex-row w-full">
+      <div className="bg-gray-700 w-full h-full hidden md:flex md:flex-col items-center justify-center text-3xl font-normal text-center">
+        <div className="p-page flex flex-col gap-5">
+          <p className="font-extrabold text-8xl pb-10">
+            ¡Bienvenidos/as!
+            <br />
+          </p>
+          <p>
+            Si llegaste hasta aca, significa que estas invitado a compartir con
+            nosotros uno de los días mas especiales de nuestras vidas juntos:
+            <b> ¡Nuestra boda!</b>
+          </p>
+          <p>
+            Estamos muy felices y esperamos verte allí para celebrar juntos,
+            reír bailar y seguir creando recuerdos. Los queremos mucho y
+            bienvenidos a nuestra nueva página.
+          </p>
+          <p className="flex flex-row gap-2 text-center w-full justify-center items-center">
+            Aquí tendrás acceso a info util como:
+            <Link href="#vestimenta">
+              <Button variant={'outline'}>Código de vestimenta</Button>
+            </Link>
+            <Link href="#direcciones">
+              <Button variant={'outline'}>Información y direcciones</Button>
+            </Link>
+            <Link href="#regalos">
+              <Button variant={'outline'}>Regalos</Button>
+            </Link>
+          </p>
+          <p>
+            Si tenes alguna duda no te la guardes, escribinos! Con mucha alegría
+            y cariño los esperan pronto, <b>Ruben y Mica</b>
+          </p>
+        </div>
+      </div>
+
+      <div className="h-full relative">
+        <div className="flex md:hidden w-5/6 absolute top-1/3 justify-self-center">
+          <Card className="opacity-85 w-full">
+            <CardContent className="text-center flex flex-col gap-5 font-bold text-xs">
+              <p className="font-extrabold text-2xl">¡Bienvenidos/as!</p>
+              <p>
+                Si llegaste hasta aca, significa que estas invitado a compartir
+                con nosotros uno de los días mas especiales de nuestras vidas
+                juntos:
+                <b> ¡Nuestra boda!</b>
+              </p>
+              <p>
+                Estamos muy felices y esperamos verte allí para celebrar juntos,
+                reír bailar y seguir creando recuerdos. Los queremos mucho y
+                bienvenidos a nuestra nueva página.
+              </p>
+              <p className="flex flex-col md:flex-row gap-2 text-center justify-center items-center">
+                Aquí tendrás acceso a info util como:
+                <Link href="#vestimenta">
+                  <Button variant={'outline'} className="text-xs">
+                    Código de vestimenta
+                  </Button>
+                </Link>
+                <Link href="#direcciones">
+                  <Button variant={'outline'} className="text-xs">
+                    Información y direcciones
+                  </Button>
+                </Link>
+                <Link href="#regalos">
+                  <Button variant={'outline'} className="text-xs">
+                    Regalos
+                  </Button>
+                </Link>
+              </p>
+              <p>
+                Si tenes alguna duda no te la guardes, escribinos! Con mucha
+                alegría y cariño los esperan pronto, <b>Ruben y Mica</b>
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <Image
+          src={'/header.jpg'}
+          className="w-full h-full object-cover"
+          width={1920}
+          height={1920}
+          alt="Micaela y Rubén"
+        />
+      </div>
+    </section>
+  );
+}
